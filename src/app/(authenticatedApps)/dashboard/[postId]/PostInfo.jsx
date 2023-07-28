d9231606690ed7a2d3492135a9d35fcc21f47a73
rajatgautam755421 <rg755421@gmail.com>
2023-07-28 09:29:59 +0545

    -UI completion

--- src/app/(authenticatedApps)/dashboard/[postId]/PostInfo.jsx
+++ src/app/(authenticatedApps)/dashboard/[postId]/PostInfo.jsx
@@ -1,18 +1,50 @@
 "use client";
 
+import AddEditPost from "@/components/AddEditPost";
 import CardCommom from "@/components/Card";
-import React, { useEffect, useState } from "react";
+import { POST_FIELDS } from "@/helpers/constant";
+import { useState } from "react";
+import { Form } from "react-bootstrap";
 
 const PostInfo = ({ data }) => {
   const [item, setItem] = useState(data);
+  const [itemMetaData, setItemMetaData] = useState(null);
 
   return (
-    <CardCommom
-      title={item.title}
-      description={item.description}
-      image={item.image}
-      buttonText={"Edit"}
-    />
+    <>
+      {itemMetaData && (
+        <AddEditPost
+          show={itemMetaData}
+          onClose={() => setItemMetaData(null)}
+          title="Edit Post"
+        >
+          {POST_FIELDS.map(({ key, label }) => {
+            return (
+              <Form.Group className="mb-3" key={key}>
+                <Form.Label>{label}</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={itemMetaData[key]}
+                  onChange={(e) =>
+                    setItemMetaData({
+                      ...itemMetaData,
+                      [key]: e.target.value,
+                    })
+                  }
+                />
+              </Form.Group>
+            );
+          })}
+        </AddEditPost>
+      )}
+      <CardCommom
+        title={item.title}
+        description={item.description}
+        image={item.image}
+        buttonText={"Edit"}
+        onClick={() => setItemMetaData(item)}
+      />
+    </>
   );
 };
 
